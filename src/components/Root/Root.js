51364b7453c34bd79eb7fd13b2d43e99a5ff3bdf
Anna Jezierska <annamajezierska@gmail.com>
2019-02-22 16:57:05 +0100

    map and login components created

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import MainMenu from '../MainMenu';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "../Login";
+import Map from "../Map";
 
 class Root extends Component {
   render() {
     return (
       <Router>
-        <div style={{ display: 'flex', width: '100%' }}>
-          <div style={{ width: '20%', background: '#eee' }}>
-            <MainMenu />
-          </div>
-
+        <div style={{ display: "flex", width: "100%" }}>
+        
           <div
             style={{
               flexGrow: 1,
-              minHeight: '100vh',
+              minHeight: "100vh",
               padding: 20,
-              boxSizing: 'border-box',
+              boxSizing: "border-box"
             }}
           >
-          <Route path="/" render={() => 'Hi'} />
-            
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/map" component={Map} />
           </div>
         </div>
       </Router>
