73ac3687b63bcb278cdc2f4e7e621da2b134059b
Anna Jezierska <annamajezierska@gmail.com>
2019-02-25 15:40:48 +0100

    login page corrected

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,7 +6,37 @@ import "./Login.css";
 class Login extends Component {
   render() {
     return (
-      <div className="login">
+      <>
+        <h1>Log in here</h1>
+        <div className="row">
+          <form className="col s12">
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="password" type="password" className="validate" />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="email" type="email" className="validate" />
+                <label htmlFor="email">Email</label>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="signup-container">
+          <h1>Don't have account?</h1>
+          <button>Sign up!</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Login;
+
+{
+  /* <div className="login">
         <div className="logo">
           <img src={homezoonew} alt="logo" />
         </div>
@@ -20,9 +50,5 @@ class Login extends Component {
           <button>I have a dog</button>
           <button>I want to be a pet-sitter</button>
         </div>
-      </div>
-    );
-  }
+      </div> */
 }
-
-export default Login;
