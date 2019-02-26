3f2664cd6a493e96ad5c5f4b0d016a1c79f7375a
Piotr Rynkowski <rynek0@onet.eu>
2019-02-26 14:46:42 +0100

    adding petsiters

--- src/components/ProfessionSelection/ProfessionSelection.js
+++ src/components/ProfessionSelection/ProfessionSelection.js
@@ -11,7 +11,7 @@ class ProfessionSelection extends Component {
       <div>
       <Button color="secondary" size="lg" tag={Link} to="/petowner">Pet Owner </Button></div>
       <p>Describe your own profile, so that other users can easily leave you your pet </p>
-      <div><Button color="secondary" size="lg">Pet Sitter</Button></div>
+      <div><Button color="secondary" size="lg" tag={Link} to="/petsiter">Pet Sitter</Button></div>
       <p>Show and describe your pet so that no one will be surprised by his attitude and appearance</p>
       
       </div>
