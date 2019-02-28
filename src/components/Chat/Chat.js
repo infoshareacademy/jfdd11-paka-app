1b42eb56fd27efe4ed170a380c1f5c10cace1147
Piotr Rynkowski <rynek0@onet.eu>
2019-02-28 15:47:30 +0100

    chat from

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,19 +1,50 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 
 import Auth from '../Auth/Auth';
-import './Chat.css'
+import './Chat.css';
 import { withAuth } from '../../context/AuthContext';
 import firebase from 'firebase';
 
-
 class Chat extends Component {
   state = {
-   message: '',
-   
-    
+    message: '',
+    messages: null,
+    users: null,
+  };
+
+  readMessages = snapshot => {
+    this.setState({
+      messages: snapshot.val(),
+    });
+  };
+
+  readUsers = snapshot => {
+    this.setState({
+      users: snapshot.val(),
+    });
+  };
+  componentDidMount() {
+    firebase
+      .database()
+      .ref('messages')
+      .on('value', this.readMessages);
+
+    firebase
+      .database()
+      .ref('users')
+      .on('value', this.readUsers);
   }
-  
+  componentWillUnmount() {
+    firebase
+      .database()
+      .ref('messages')
+      .off('value', this.readMessages);
+
+    firebase
+      .database()
+      .ref('users')
+      .off('value', this.readUsers);
+  }
 
   handleMessageChange = event => {
     this.setState({
@@ -23,32 +54,55 @@ class Chat extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-   
+    
+    // Remember that setState is async
+    this.setState(({
+      message: ''
+    }))
 
     firebase
-    .database()
-    .ref('messages')
-    .push({
-      content: this.state.message,
-      authorId: this.props.authContext.user.uid,
-      createdAt: firebase.database.ServerValue.TIMESTAMP
-    });
+      .database()
+      .ref('messages')
+      .push({
+        content: this.state.message,
+        authorId: this.props.authContext.user.uid,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+      });
   };
- 
 
   render() {
-   
+    const { users } = this.state;
+    const messagesArray = Object.entries(this.state.messages || {}).map(
+      ([id, value]) => ({ id, ...value })
+    ).reverse();
+
     return (
       <div className="Chat">
-      <h1>Chat</h1>
-      <Auth cover={() => <p>Chat is only for logged in users.</p>}>
-      <form onSubmit={this.handleSubmit}>
-      <input value={this.state.message}
-      onChange={this.handleMessageChange}/>
-      </form>
-      </Auth>
+        <h1>Chat</h1>
+        <Auth cover={() => <p>Chat is only for logged in users.</p>}>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              value={this.state.message}
+              onChange={this.handleMessageChange}
+            />
+          </form>
+        </Auth>
 
-    </div>
+        <div>
+          {messagesArray.map(message => (
+            <p key={message.id}>
+              {message.createdAt}
+              <strong>
+                {(users &&
+                  users[message.authorId] &&
+                  users[message.authorId].name) ||
+                  message.authorId}
+              </strong>
+              {message.content}
+            </p>
+          ))}
+        </div>
+      </div>
     );
   }
 }
