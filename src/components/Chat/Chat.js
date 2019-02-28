6c21310217d8f4a7db7f7c74b0bb839e1d835e05
Piotr Rynkowski <rynek0@onet.eu>
2019-02-28 14:41:51 +0100

    starting chat second time

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,45 +1,48 @@
 import React, { Component } from 'react'
 
+
+import Auth from '../Auth/Auth';
 import './Chat.css'
+import { withAuth } from '../../context/AuthContext';
+import firebase from 'firebase';
+
 
 class Chat extends Component {
   state = {
-    messages: [
-      {
-        id: '',
-        author: '',
-        messageText: '',
-      },
-    ],
+   message: '',
+    
   }
-  // handleSubmit = event => {
-  //   event.preventDefault();
-  // }
 
-  // handleChange = event => {
-  //   const fieldAuthor = event.target.author;
-  //   const value =
-  //   event.target.type === 'checkbox'
-  //   ? event.target.checked
-  //   : event.target.value;
+  handleMessageChange = event => {
+    this.setState({
+      message: event.target.value,
+    });
+  };
 
-  //   this.state({
-  //     [fieldAuthor]: value
-  //   })
-  // }
+  handleSubmit = event => {
+    event.preventDefault();
 
+    firebase
+    .database()
+    .ref('messasges')
+    .push({
+      content: this.state.message,
+      authorId: this.props.authContext.user.uid,
+      createdAAt: firebase.database.ServerValue.TIMESTAMP
+    });
+  };
+ 
 
   render() {
     return (
       <div className="Chat">
-      <h1>Messages</h1>
-      <p>
-        {this.state.messages.map(message =>
-          <p key={message.id}>
-          <p>{message.author}</p>
-          <p>{message.messageText}</p>
-          </p>)}
-      </p>
+      <h1>Chat</h1>
+      <Auth cover={() => <p>Chat is only for logged in users.</p>}>
+      <form onSubmit={this.state.message}>
+      <input value={this.state.message}
+      onChange={this.handleMessageChange}/>
+      </form>
+      </Auth>
 
     </div>
     );
