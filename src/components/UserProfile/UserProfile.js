99bd6b3881b8493275c3fb2dc666bc0822e03c72
Anna Jezierska <annamajezierska@gmail.com>
2019-03-02 10:20:22 +0100

    checkboxes to browse users added

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
-import { Card, Button, CardBody, CardLink,CardSubtitle ,TabContent, TabPane, Nav, NavItem, NavLink, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { Card, Form, FormGroup, Label, Input, CardBody, CardLink,CardSubtitle ,TabContent, TabPane, Nav, NavItem, NavLink, CardTitle, CardText, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
 import MyMap from '../MyMap'
 
@@ -11,11 +11,15 @@ class UserProfile extends Component {
     users: [],
     activeTab: '1',
     searchPhrase: '',
+    daycare: false,
+    housesitting: false,
+    schedule: false,
+    visits: false
   };
 
   handleChange = event => {
     this.setState({
-      searchPhrase: event.target.value
+      searchPhrase: event.target.value,
     });
   }
 
@@ -24,6 +28,13 @@ class UserProfile extends Component {
     activeTab: tab
   })
 }
+handleCheckboxChange = (e) => {
+  console.log(e.target.checked, e.target.id, e.target)
+   this.setState({
+     [e.target.id]: e.target.checked
+   })
+}
+
   componentDidMount() {
      
     firebase.auth().onAuthStateChanged(currentUser => {
@@ -39,9 +50,16 @@ class UserProfile extends Component {
       }
     })
   }
-
+ 
+  
   render() {
-    console.log(this.state)
+    const { users, housesitting, daycare, schedule, visits } = this.state
+    const isDayCare = daycare ? (user => user.daycare) : user => [...users]
+  const isHouseSitting = housesitting ? (user => user.housesitting) : user => [...users]
+   const isVisiting = visits ? (user => user.visits) : user => [...users]
+    const isWalking = schedule ? (user => user.schedule) : user => [...users]
+   const userId = firebase.auth().currentUser.uid
+    
     return (
       <div style={{display: 'flex', 
       flexDirection: 'column'
@@ -73,6 +91,30 @@ class UserProfile extends Component {
                 <Card body>
                   <CardTitle>Advanced search</CardTitle>
                   <input value={this.state.searchPhrase} onChange={this.handleChange} placeholder='Search by name, surname, city and age'/>
+                
+                  <Form>
+        <FormGroup check inline>
+          <Label check>
+            <Input type="checkbox" id='daycare' checked={daycare} onChange={this.handleCheckboxChange}/> Day Care at petsitter's home
+          </Label>
+        </FormGroup>
+        <FormGroup check inline>
+          <Label check>
+             <Input type="checkbox" id='housesitting' checked={housesitting} onChange={this.handleCheckboxChange}/> House sitting at your place
+          </Label>
+        </FormGroup>
+        <FormGroup check inline>
+          <Label check>
+             <Input type="checkbox" id='schedule' checked={schedule} onChange={this.handleCheckboxChange}/> House sitting at your place
+          </Label>
+        </FormGroup>
+        <FormGroup check inline>
+          <Label check>
+             <Input type="checkbox" id='visits' checked={visits} onChange={this.handleCheckboxChange}/> House sitting at your place
+          </Label>
+        </FormGroup>
+      </Form>
+
                 </Card>
               </Col>
             </Row>
@@ -90,6 +132,10 @@ class UserProfile extends Component {
                   user.name + user.surname + user.adress + user.age
                 ).toLocaleLowerCase(),
               }))
+              .filter(isHouseSitting)
+              .filter(isDayCare)
+              .filter(isVisiting)
+              .filter(isWalking)
               .filter(user =>
                 user.searchData.includes(
                   this.state.searchPhrase.toLocaleLowerCase()
@@ -100,6 +146,10 @@ class UserProfile extends Component {
         <CardBody>
           <CardTitle>{user.name}</CardTitle>
           <CardSubtitle>{user.age}</CardSubtitle>
+          <p>housesitting: {user.housesitting ? <span>yes</span> : <span>no</span>}</p>
+          <p>Day care: {user.daycare ? <span>yes</span> : <span>no</span>}</p>
+          <p>Available for walk: {user.schedule ? <span>yes</span> : <span>no</span>}</p>
+          <p>Availavle to drop in: {user.visits ? <span>yes</span> : <span>no</span>}</p>
         </CardBody>
         <div style={{ textAlign: 'center'}}>
         <img src={user.photo + '&size=150x150'} alt='user'/>
