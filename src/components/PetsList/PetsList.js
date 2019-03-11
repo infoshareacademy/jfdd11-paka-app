e37f1e86aaacc99963ff8ed9e411787a6af0a2f5
Adam Kowalski <adamkrzysztof1@wp.pl>
2019-03-11 16:25:59 +0100

    Pets List view changed, features removed,dogs data added

--- src/components/PetsList/PetsList.js
+++ src/components/PetsList/PetsList.js
@@ -2,27 +2,11 @@ import React, { Component } from 'react'
 import firebase from "firebase";
 import {
   Card,
-  Form,
-  FormGroup,
-  Label,
-  Input,
   CardBody,
-  UncontrolledCollapse,
-  Button,
-  CardLink,
-  CardSubtitle,
-  TabContent,
-  TabPane,
-  Nav,
-  NavItem,
-  NavLink,
+  
   CardTitle,
-  CardText,
-  Row,
-  Col
 } from "reactstrap";
-import classnames from "classnames";
-import { Link } from "react-router-dom";
+
 import './PetsList.css'
 
 class PetsList extends Component {
@@ -40,25 +24,6 @@ state = {
   file: null
 };
 
-/*
-handleChange = event => {
-  this.setState({
-    searchPhrase: event.target.value
-  });
-};
-
-toggle = tab => {
-  this.setState({
-    activeTab: tab
-  });
-};
-handleCheckboxChange = e => {
-  this.setState({
-    [e.target.id]: e.target.checked
-  });
-};
-*/
-
 componentDidMount() {
   firebase.auth().onAuthStateChanged(currentpet => {
     if (currentpet !== null) {
@@ -75,17 +40,7 @@ componentDidMount() {
   });
 }
 
-
-
   render() { 
-    const { pets, housesitting, dayCare, dogWalking
-  , dropIn } = this.state;
-    /*const isdayCare = dayCare ? pet => pet.dayCare : pet => [...pets];
-    const isHouseSitting = housesitting ? pet => pet.housesitting : pet => [...pets];
-    const isVisiting = dropIn ? pet => pet.dropIn : pet => [...pets];
-    const isWalking = dogWalking
- ? pet => pet.dogWalking
- : pet => [...pets];*/
 
   return (
     <div
@@ -93,39 +48,17 @@ componentDidMount() {
       className="PetsList">
       <div>
         
-            {/* <Row>
-            <Col sm="12">
-              <h4> </h4>
-            </Col>
-          </Row> */}
+            {}
             <div >
               {this.state.pets
                 .map(pet => (
                   <Card key={pet.id}>
                     <CardBody>
                       <CardTitle>{pet.dogsname}</CardTitle>
-                      <CardSubtitle>{pet.age}</CardSubtitle>
-                      <p>
-                        housesitting:{" "}
-                        {pet.dayNightCare ? (
-                          <span>yes</span>
-                        ) : (
-                          <span>no</span>
-                        )}
-                      </p>
-                      <p>
-                        Day care:{" "}
-                        {pet.dayCare ? <span>yes</span> : <span>no</span>}
-                      </p>
-                      <p>
-                        Available for walk:{" "}
-                        {pet.dogWalking
-                       ? <span>yes</span> : <span>no</span>}
-                      </p>
-                      <p>
-                        Available to drop in:{" "}
-                        {pet.dropIn ? <span>yes</span> : <span>no</span>}
-                      </p>
+                      <CardTitle>Age: {pet.age}</CardTitle>
+                      <CardTitle>Breed: {pet.breed}</CardTitle>
+                      <CardTitle>Gender: {pet.gender}</CardTitle>
+                      <CardTitle>Description: {pet.description}</CardTitle>
                     </CardBody>
                     <div style={{ textAlign: "center" }}>
                       <img src={pet.photo} alt="Soon i will add my dog's photo:)" style={{width: '100%'}} />
@@ -134,13 +67,9 @@ componentDidMount() {
                   </Card>
                 ))}
             </div>
-          
-          
-        
       </div>
     </div>
   );
-
 
   }
 }
