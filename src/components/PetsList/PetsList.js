5044aa4386949411fcbdde45676fb40350992239
Adam Kowalski <adamkrzysztof1@wp.pl>
2019-03-12 16:27:41 +0100

    pet owner name visible at user's pets list

--- src/components/PetsList/PetsList.js
+++ src/components/PetsList/PetsList.js
@@ -1,73 +1,96 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import firebase from "firebase";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardBody, CardTitle } from "reactstrap";
 
-import './PetsList.css'
+import "./PetsList.css";
 
 class PetsList extends Component {
-state = {
-  pets: [],
-  dogsname: "",
-  age: "",
-  breed: "",
-  gender: "",
-  description: '',
-  file: null
-};
+  state = {
+    pets: [],
+    users: [],
+    owner: "",
+    dogsname: "",
+    age: "",
+    breed: "",
+    gender: "",
+    description: "",
+    file: null
+  };
 
-componentDidMount() {
-  firebase.auth().onAuthStateChanged(currentpet => {
-    if (currentpet !== null) {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(currentpet => {
+      if (currentpet !== null) {
+        firebase
+          .database()
+          .ref(`pets`)
+          .once("value")
+          .then(snapshot => snapshot.val())
+          .then(pets => {
+            this.setState({
+              pets: Object.entries(pets || {}).map(([id, value]) => ({
+                id,
+                ...value
+              }))
+            });
+          });
 
-      firebase
-        .database()
-        .ref(`pets`)
-        .once("value")
-        .then(snapshot => snapshot.val())
-        .then(pets => {
-          this.setState({ pets: Object.entries(pets || {}).map(([id, value]) => ({ id, ...value })) });
-        });
-    }
-  });
-}
+        firebase
+          .database()
+          .ref(`users`)
+          .once("value")
+          .then(snapshot => snapshot.val())
+          .then(users => {
+            this.setState({
+              users: Object.entries(users || {}).map(([id, value]) => ({
+                id,
+                ...value
+              }))
+            });
+          });
+      }
+    });
+  }
 
-  render() { 
+  getOwnerById = (ownerId) => {
+   
+    const user = this.state.users.find(user=>user.id ===ownerId)
+    return (user && user.name ? user.name : "not registered")
+  }
 
-  return (
-    <div
-      style={{ display: "flex", flexDirection: "column" }}
-      className="PetsList">
-      <div>
-        
-            <div >
-              {this.state.pets
-                .map(pet => (
-                  <Card key={pet.id}>
-                    <CardBody>
-                      <CardTitle>{pet.dogsname}</CardTitle>
-                      <CardTitle>Age: {pet.age}</CardTitle>
-                      <CardTitle>Breed: {pet.breed}</CardTitle>
-                      <CardTitle>Gender: {pet.gender}</CardTitle>
-                      <CardTitle>Description: {pet.description}</CardTitle>
-                    </CardBody>
-                    <div style={{ textAlign: "center" }}>
-                      <img src={pet.photo} alt="Soon i will add my dog's photo:)" style={{width: '100%'}} />
-                    </div>
-                   
-                  </Card>
-                ))}
-            </div>
+  render() {
+    return (
+      <div
+        style={{ display: "flex", flexDirection: "column" }}
+        className="PetsList"
+      >
+        <div>
+          <div>
+            {this.state.pets.map(pet => (
+              <Card key={pet.id}>
+                <CardBody>
+                  <CardTitle>Dog's name: {pet.dogsname}</CardTitle>
+                  <CardTitle>Age: {pet.age}</CardTitle>
+                  <CardTitle>Breed: {pet.breed}</CardTitle>
+                  <CardTitle>Gender: {pet.gender}</CardTitle>
+                  <CardTitle>Description: {pet.description}</CardTitle>
+                  <CardTitle>
+                    Owner: {this.getOwnerById(pet.ownerId)}
+                  </CardTitle>
+                </CardBody>
+                <div style={{ textAlign: "center" }}>
+                  <img
+                    src={pet.photo}
+                    alt="Soon i will add my dog's photo:)"
+                    style={{ width: "100%" }}
+                  />
+                </div>
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-  );
-
+    );
   }
 }
 
-export default PetsList
-
-
+export default PetsList;
