4bba95f149e9bfe2ee8742c8b7958616eb7e2129
Adam Kowalski <adamkrzysztof1@wp.pl>
2019-03-12 16:46:49 +0100

    custom css in comments, reactstrap css in rule

--- src/components/PetOwner/PetOwner.js
+++ src/components/PetOwner/PetOwner.js
@@ -1,120 +1,137 @@
-import React, { Component } from 'react'
-import { Input, Card, CardImg } from 'reactstrap';
-import './PetOwner.css'
-
+import React, { Component } from "react";
+import { Input, Card, CardImg, FormGroup } from "reactstrap";
+import "./PetOwner.css";
 
 const initialState = {
-  name: '',
-  surname: '',
-  dogsname: '',
-  age: '',
-  gender: '',
-  breed: '',
-  description: '',
+  name: "",
+  surname: "",
+  dogsname: "",
+  age: "",
+  gender: "",
+  breed: "",
+  description: "",
   file: null
 };
 
 class PetOwner extends Component {
   state = initialState;
 
-  handleName = (event) => {
-    const { onNameChange } = this.props
-    onNameChange && onNameChange(event.target.value)
-  }
+  handleName = event => {
+    const { onNameChange } = this.props;
+    onNameChange && onNameChange(event.target.value);
+  };
 
-  handleSurname = (event) => {
-    const { onSurnameChange } = this.props
-    onSurnameChange && onSurnameChange(event.target.value)
-  }
+  handleSurname = event => {
+    const { onSurnameChange } = this.props;
+    onSurnameChange && onSurnameChange(event.target.value);
+  };
 
-  handleDogsname = (event) => {
-    const { onDogsnameChange } = this.props
-    onDogsnameChange && onDogsnameChange(event.target.value)
-  }
+  handleDogsname = event => {
+    const { onDogsnameChange } = this.props;
+    onDogsnameChange && onDogsnameChange(event.target.value);
+  };
 
-  handleAge = (event) => {
-    const { onAgeChange } = this.props
-    onAgeChange && onAgeChange(event.target.value)
-  }
+  handleAge = event => {
+    const { onAgeChange } = this.props;
+    onAgeChange && onAgeChange(event.target.value);
+  };
 
-  handleGender = (event) => {
-    const { onGenderChange } = this.props
-    onGenderChange && onGenderChange(event.target.value)
-  }
+  handleGender = event => {
+    const { onGenderChange } = this.props;
+    onGenderChange && onGenderChange(event.target.value);
+  };
 
-  handleBreed = (event) => {
-    const { onBreedChange } = this.props
-    onBreedChange && onBreedChange(event.target.value)
-  }
+  handleBreed = event => {
+    const { onBreedChange } = this.props;
+    onBreedChange && onBreedChange(event.target.value);
+  };
 
-  handleDescriptionChange = (event) => {
-    const { onDescriptionChange } = this.props
-    onDescriptionChange && onDescriptionChange(event.target.value)
-  }
+  handleDescriptionChange = event => {
+    const { onDescriptionChange } = this.props;
+    onDescriptionChange && onDescriptionChange(event.target.value);
+  };
 
-  handleFileSelected = (event) => {
-    const { onFileSelected } = this.props
-    onFileSelected && onFileSelected(event.target.files[0])
-    this.setState({file :URL.createObjectURL( event.target.files[0])})
-  }
+  handleFileSelected = event => {
+    const { onFileSelected } = this.props;
+    onFileSelected && onFileSelected(event.target.files[0]);
+    this.setState({ file: URL.createObjectURL(event.target.files[0]) });
+  };
 
   render() {
-    const { file } = this.state
+    const { file } = this.state;
     return (
       <div className="PetOwnerPage">
-
-          Info about your dog
-          <div className="inputWraper">
-          
+        Info about your dog
+        <div className="inputWraper">
           <div className="inputSmallWraper">
-           
-              <input
-              className="inputSmall"
-              type="text"
-              placeholder=" Dog's name"
-              name="dogsname"
-              onChange={this.handleDogsname}
-            />
+            <FormGroup>
+              <Input
+                className="inputSmall"
+                type="text"
+                placeholder=" Dog's name"
+                name="dogsname"
+                onChange={this.handleDogsname}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                className="inputSmall"
+                type="number"
+                min="1"
+                max="25"
+                placeholder=" Age"
+                name="age"
+                onChange={this.handleAge}
+              />
+            </FormGroup>
 
-            <input
-              className="inputSmall"
-              type="number" min="1" max="25"
-              placeholder=" Age"
-              name="age"
-              onChange={this.handleAge}
-            />
+            <FormGroup>
+              <Input
+                className="inputSmall"
+                type="text"
+                placeholder=" Gender"
+                name="gender"
+                onChange={this.handleGender}
+              />
+            </FormGroup>
 
-            <input
-              className="inputSmall"
-              type="text"
-              placeholder=" Gender"
-              name="gender"
-              onChange={this.handleGender}
-            />
+            <FormGroup>
+              <Input
+                className="inputSmall"
+                type="text"
+                placeholder=" Gender"
+                name="gender"
+                onChange={this.handleGender}
+              />
+            </FormGroup>
 
-            <input
-              className="inputSmall"
-              type="text"
-              placeholder=" Breed"
-              name="breed"
-              onChange={this.handleBreed}
-            />
-
+            <FormGroup>
+              <Input
+                className="inputSmall"
+                type="text"
+                placeholder=" Breed"
+                name="breed"
+                onChange={this.handleBreed}
+              />
+            </FormGroup>
           </div>
-          
+          <div className="inputFile">
           <Card>
-           <CardImg src={file} alt="" />
-            <Input type="file"  name="file" id="exampleFile" onChange={this.handleFileSelected }/>
-            </Card>
+            <CardImg src={file} alt="" />
+            <Input
+              type="file"
+              name="file"
+              id="exampleFile"
+              onChange={this.handleFileSelected}
+            />
+          </Card>
+          </div>
         </div>
         <p>Short description of your dog:</p>
-        <textarea rows="4" onChange={this.handleDescriptionChange}></textarea>
-
-
-
+        <textarea rows="4" onChange={this.handleDescriptionChange} />
       </div>
-    )
+    );
   }
 }
 
-export default PetOwner
+export default PetOwner;
